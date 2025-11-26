24cfc6bdd42f4f4ce2a209745ed38cdadfa15064
tarasiuknazar7-svg <tarasiuknazar7@gmail.com>
2025-11-26 01:02:08 +0000

    Edits v1.0

---
+++ Pages/PolicyPage/script.js
@@ -0,0 +1,6 @@
+const burger = document.getElementById('burger');
+const sideMenu = document.getElementById('sideMenu');
+
+burger.addEventListener('click', () => {
+  sideMenu.classList.toggle('active');
+});
--- docs/script.js
+++ docs/script.js
@@ -4,3 +4,11 @@ const sideMenu = document.getElementById('sideMenu');
 burger.addEventListener('click', () => {
   sideMenu.classList.toggle('active');
 });
+
+document.getElementById('go1').addEventListener('click', function() {
+    window.location.href = '../Pages/CoursePage/course.html';
+});
+
+document.getElementById('go2').addEventListener('click', function() {
+    window.location.href = '../Pages/NewsPage/news.html';
+});
